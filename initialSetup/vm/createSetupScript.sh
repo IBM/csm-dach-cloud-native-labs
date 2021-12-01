ccdfa2edc0c22ac2fbbec559330922c259a443b5
RapTho <raphael.tholl@protonmail.com>
2021-12-01 16:51:34 +0100

    removed git clone to home folders

--- initialSetup/vm/createSetupScript.sh
+++ initialSetup/vm/createSetupScript.sh
@@ -17,10 +17,6 @@ tar -xf oc.tar
 chmod 777 oc
 mv oc /usr/bin/
 #
-# Get latest version of Github repository
-cd /root
-git clone https://github.com/IBM/csm-dach-cloud-native-labs.git
-#
 # Create group + user
 groupadd devUsers
 chown root:devUsers /usr/bin/oc
@@ -28,7 +24,5 @@ for NUM in $(seq 1 $NUMBER_USERS)
 	do
 	useradd -G devUsers user$NUM
 	echo user${NUM}:${PASSWORD} | chpasswd
-	cp -R /root/csm-dach-cloud-native-labs /home/user${NUM}/
-	chown -R user${NUM}:user${NUM} /home/user${NUM}/
 	echo "Created user${NUM}"
 	done
