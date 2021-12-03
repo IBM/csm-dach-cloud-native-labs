3558e189e10c63d90a9621e9a0b63c89c2812b26
RapTho <raphael.tholl@protonmail.com>
2021-12-03 16:51:06 +0100

    fixed indent

--- initialSetup/openshift/createServiceAccountUserScript.sh
+++ initialSetup/openshift/createServiceAccountUserScript.sh
@@ -8,6 +8,6 @@ NUMBER_USERS=15
 for NUM in $(seq 1 $NUMBER_USERS)
 	do
 	oc create serviceaccount -n default user$NUM
-    	oc create clusterrolebinding user$NUM --clusterrole=self-provisioners --serviceaccount=default:user$NUM
-    	oc adm policy add-cluster-role-to-user self-provisioner system:serviceaccount:default:user$NUM
+    oc create clusterrolebinding user$NUM --clusterrole=self-provisioners --serviceaccount=default:user$NUM
+    oc adm policy add-cluster-role-to-user self-provisioner system:serviceaccount:default:user$NUM
 	done
