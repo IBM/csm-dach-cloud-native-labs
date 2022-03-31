e560aebba251e07f09a32ecfe91c9af27f3774da
Raphael Tholl <48417580+RapTho@users.noreply.github.com>
2022-03-31 11:50:52 +0200

    removed clusterrolebinding self-provisioner

--- initialSetup/openshift/createServiceAccountUserScript.sh
+++ initialSetup/openshift/createServiceAccountUserScript.sh
@@ -9,7 +9,6 @@ oc adm groups new externals
 for NUM in $(seq 1 $NUMBER_USERS)
 	do
 	oc create serviceaccount -n default user$NUM
-    # oc create clusterrolebinding user$NUM --clusterrole=self-provisioners --serviceaccount=default:user$NUM
 	oc adm groups add-users externals user$NUM
 	done
 oc adm policy add-cluster-role-to-group self-provisioner externals
