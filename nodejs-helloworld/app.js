63a4b4ba1c179ade054c41c9b7a95f0ab3a12a49
Raphael Tholl <48417580+RapTho@users.noreply.github.com>
2023-06-28 13:38:20 +0200

    reset msg

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello CoBa!');
+  res.send('Hello World!');
 });
 
 app.listen(8080, function () {
