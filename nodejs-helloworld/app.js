e0ec220e5b6f250bef05066270e7db871b0cb187
Rhewid <102160136+RhettIBM@users.noreply.github.com>
2023-11-22 13:50:28 +0100

    Update app.js

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello Openshift!');
 });
 
 app.listen(8080, function () {
