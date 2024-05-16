0ad5f8c2aea424dd9f2d164851ba4c79443dcdd9
Rhewid <102160136+RhettIBM@users.noreply.github.com>
2024-05-16 14:10:55 +0200

    Update app.js

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello all! This time rebuilding on the console. With the Webhook in place.');
+  res.send('Hello all! This time for everybody. A new change!');
 });
 
 app.listen(8080, function () {
