775e5b08bd3846963690272b248b945a8fddbac6
Rhewid <102160136+RhettIBM@users.noreply.github.com>
2024-10-16 11:02:13 +0200

    Update getServiceAccountCreds.sh
    now uses .dockercfg, needed adjustment to get the token from the sa.

--- initialSetup/openshift/getServiceAccountCreds.sh
+++ initialSetup/openshift/getServiceAccountCreds.sh
@@ -8,6 +8,6 @@ NUMBER_USERS=15
 SERVER=$(oc status | grep -m1 "In project" | awk '{print$6}')
 for NUM in $(seq 1 $NUMBER_USERS)
 	do
-    TOKEN=$(oc -n default get secret $(oc get secret -n default | grep -m1 user${NUM}-token | awk '{print$1}') -o jsonpath='{.data.token}' | base64 --decode)
+    TOKEN=$(oc -n default get secret $(oc get secret -n default | grep -m1 user1-dockercfg | awk '{print $1}') -o jsonpath='{.data.\.dockercfg}' | base64 --decode | jq -r 'to_entries[0].value.auth' | base64 --decode | cut -d':' -f2)
 	echo -e "Token User $NUM:\noc login --token=$TOKEN --server=$SERVER\n\n"
 	done
