9bef87feb85b9c63a505cc48d7d275947430e794
Rhewid <102160136+RhettIBM@users.noreply.github.com>
2024-11-21 14:57:50 +0100

    Update getServiceAccountCreds.sh

--- initialSetup/openshift/getServiceAccountCreds.sh
+++ initialSetup/openshift/getServiceAccountCreds.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-#
+# sh getServiceAccountCreds.sh >> /tmp/login
+# sh getSA.sh >> /tmp/login
 # Report bugs at: https://github.com/IBM/csm-dach-cloud-native-labs/issues
 #
 # Choose how many users you want
 NUMBER_USERS=15
 #
 SERVER=$(oc status | grep -m1 "In project" | awk '{print$6}')
-for NUM in $(seq 1 $NUMBER_USERS)
-	do
-    TOKEN=$(oc -n default get secret $(oc get secret -n default | grep -m1 user1-dockercfg | awk '{print $1}') -o jsonpath='{.data.\.dockercfg}' | base64 --decode | jq -r 'to_entries[0].value.auth' | base64 --decode | cut -d':' -f2)
-	echo -e "Token User $NUM:\noc login --token=$TOKEN --server=$SERVER\n\n"
-	done
+for NUM in $(seq 1 $NUMBER_USERS); do
+    USER="user${NUM}"
+    TOKEN=$(oc -n default get secret $(oc get secret -n default | grep -m1 ${USER}-dockercfg | awk '{print $1}') -o jsonpath='{.data.\.dockercfg}' | base64 --decode | jq -r 'to_entries[0].value.auth' | base64 --decode | cut -d':' -f2)
+    echo -e "Token User $NUM:\noc login --token=$TOKEN --server=$SERVER\n\n"
+done
