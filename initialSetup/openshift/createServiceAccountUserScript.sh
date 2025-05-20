7751f5f1c10a676f26bb8563521b6e76333171c3
Rhewid <102160136+RhettIBM@users.noreply.github.com>
2025-05-20 17:26:57 +0200

    Update createServiceAccountUserScript.sh

--- initialSetup/openshift/createServiceAccountUserScript.sh
+++ initialSetup/openshift/createServiceAccountUserScript.sh
@@ -3,7 +3,7 @@
 # Report bugs at: https://github.com/IBM/csm-dach-cloud-native-labs/issues
 #
 # Choose how many users you want
-NUMBER_USERS=15
+NUMBER_USERS=20
 #
 for NUM in $(seq 1 $NUMBER_USERS)
 	do
